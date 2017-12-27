2a3c7db90485322958b9ba29ed531823224686b5
szm1002 <szm1002@126.com>
2017-12-27 16:17:56 +0800

    first commit: add vue-tree-menu

---
+++ src/App.vue
@@ -0,0 +1,131 @@
+<template>
+  <div id="app">
+    <ul>
+      <tree-menu v-for="item in treeData" :key="item.id" :model="item"></tree-menu>
+    </ul>
+  </div>
+</template>
+
+<script>
+import TreeMenu from './components/TreeMenu'
+import './style.css'
+
+const treeData = [
+  {
+    id: '1',
+    name: '文件夹',
+    children: [
+      {
+        id: '11',
+        name: '文件'
+      },
+      {
+        id: '12',
+        name: '文件夹',
+        children: [
+          {
+            id: '121',
+            name: '文件'
+          },
+          {
+            id: '122',
+            name: '文件'
+          },
+          {
+            id: '123',
+            name: '文件'
+          },
+          {
+            id: '124',
+            name: '文件'
+          },
+          {
+            id: '125',
+            name: '文件'
+          }
+        ]
+      },
+      {
+        id: '13',
+        name: '文件'
+      }
+    ]
+  },
+  {
+    id: '2',
+    name: '文件'
+  },
+  {
+    id: '3',
+    name: '文件夹',
+    children: [
+      {
+        id: '31',
+        name: '文件'
+      },
+      {
+        id: '32',
+        name: '空文件夹',
+        children: []
+      }
+    ]
+  },
+  {
+    id: '4',
+    name: '空文件夹',
+    children: []
+  },
+  {
+    id: '5',
+    name: '文件夹',
+    children: [
+      {
+        id: '51',
+        name: '文件'
+      },
+      {
+        id: '52',
+        name: '文件'
+      },
+      {
+        id: '53',
+        name: '文件夹',
+        children: [
+          {
+            id: '531',
+            name: '文件'
+          },
+          {
+            id: '532',
+            name: '文件'
+          },
+          {
+            id: '533',
+            name: '文件'
+          }
+        ]
+      },
+      {
+        id: '54',
+        name: '文件'
+      },
+      {
+        id: '55',
+        name: '文件'
+      }
+    ]
+  }
+]
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      treeData: treeData
+    }
+  },
+  components: {
+    TreeMenu
+  }
+}
+</script>
---
+++ src/components/TreeMenu.vue
@@ -0,0 +1,35 @@
+<template>
+  <li>
+    <span @click="toggleFolder">
+      <span v-if="isFolder" class="icon" :class="[open ? 'folder-open' : 'folder']"></span>
+      <span v-else class="icon file"></span>
+      {{ model.name }}
+    </span>
+    <ul v-if="isFolder" v-show="open">
+      <!-- recursive components -->
+      <tree-menu v-for="item in model.children" :key="item.id" :model="item"></tree-menu>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'treeMenu',
+  props: ['model'],
+  data () {
+    return {
+      open: false
+    }
+  },
+  computed: {
+    isFolder: function () {
+      return !!this.model.children
+    }
+  },
+  methods: {
+    toggleFolder: function () {
+      this.open = !this.open
+    }
+  }
+}
+</script>
